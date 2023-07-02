31a30283472f839978f47fa584f9aba4f3e63ed0
Sergey Makoveev <sergey@makoveev.info>
2023-07-03 00:05:07 +0300

    disable debug mode

--- GlobalConstants.pas
+++ GlobalConstants.pas
@@ -2,8 +2,8 @@ unit GlobalConstants;
 
 const
   // режим отладки - отображаются мины
-  DEBUG_MODE = true;
-  // DEBUG_MODE = false;
+  // DEBUG_MODE = true;
+  DEBUG_MODE = false;
 
   // ширина клетки
   WIDTH_CELL = 39;
